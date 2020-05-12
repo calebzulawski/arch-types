a4651bd73fdb3bf87bc6ed6762a9d13487db3feb
Caleb Zulawski <caleb.zulawski@gmail.com>
2020-05-11 23:29:04 -0400

    Change implementation to tuples

--- arch-types/src/lib.rs
+++ arch-types/src/lib.rs
@@ -1,6 +1,95 @@
 #![allow(non_camel_case_types)]
 use std::ops::{BitAnd, BitOr};
-use typenum::{False, True};
+use typenum::{marker_traits::Bit, False, True};
+
+macro_rules! tuple_impl {
+    // Initial state
+    {
+        $($features:ident)*
+    } => {
+        tuple_impl! {
+            @features [$($features)*]
+            (T12, T11, T10, T9, T8, T7, T6, T5, T4, T3, T2, T1)
+        }
+    };
+
+    // Unpack the next tuple
+    {
+        @features $features:tt
+        ($first:ident, $second:ident, $($rest:ident),+)
+    } => {
+        tuple_impl! {
+            @features $features
+            ($first, $second, $($rest),*)
+            ($second, $($rest),*)
+        }
+    };
+
+    // Special case the 2-tuple
+    {
+        @features [$($feature:ident)*]
+        ($first:ident, $second:ident)
+    } => {
+        unsafe impl<$first, $second> Features for ($first, $second)
+        where
+            $first: Features,
+            $second: Features,
+            $(
+                $first::$feature: BitOr<<$second as Features>::$feature>,
+                <$first::$feature as BitOr<<$second as Features>::$feature>>::Output: Bit,
+            )*
+        {
+            $(
+                type $feature = <$first::$feature as BitOr<<$second as Features>::$feature>>::Output;
+            )*
+
+            fn detect() -> Option<Self> {
+                Some(($first::detect()?, $second::detect()?))
+            }
+
+            unsafe fn new_unchecked() -> Self {
+                ($first::new_unchecked(), $second::new_unchecked())
+            }
+        }
+    };
+
+    // Implement
+    {
+        @features [$($feature:ident)*]
+        ($first:ident, $($rest:ident),+)
+        $next:tt
+    } => {
+        unsafe impl<$first, $($rest),*> Features for ($first, $($rest),*)
+        where
+            $first: Features,
+            $(
+                $rest: Features,
+            )*
+            $next: Features,
+            $(
+                $first::$feature: BitOr<<$next as Features>::$feature>,
+                <$first::$feature as BitOr<<$next as Features>::$feature>>::Output: Bit,
+            )*
+        {
+            $(
+                type $feature = <$first::$feature as BitOr<<$next as Features>::$feature>>::Output;
+            )*
+
+            fn detect() -> Option<Self> {
+                Some(($first::detect()?, $($rest::detect()?),*))
+            }
+
+            unsafe fn new_unchecked() -> Self {
+                ($first::new_unchecked(), $($rest::new_unchecked()),*)
+            }
+        }
+
+        tuple_impl! {
+            @features [$($feature)*]
+            ($($rest),*)
+        }
+    };
+}
 
 macro_rules! features {
     {
@@ -10,70 +99,92 @@ macro_rules! features {
             @detect $detect:tt
         )*
     } => {
+        /// Indicates the presence of available features.
+        pub unsafe trait Features: Copy {
+            $(
+                type $ident: Bit;
+            )*
+
+            fn detect() -> Option<Self>;
+
+            unsafe fn new_unchecked() -> Self;
+        }
+
         $(
-            #[doc = "The `\""]
+            #[doc = "The `"]
             #[doc = $detect]
-            #[doc = " \"` feature."]
-            pub struct $ident;
-
-            unsafe impl Detect for Arch<$ident> {
-                fn detect() -> Option<Self> {
-                    if $detect_macro!($detect) {
-                        Some(Self(std::marker::PhantomData))
-                    } else {
-                        None
-                    }
-                }
-
-                unsafe fn new_unchecked() -> Self {
-                    Self(std::marker::PhantomData)
-                }
-            }
+            #[doc = "` feature."]
+            /// The $detect feature.
+            #[derive(Copy, Clone, Debug)]
+            pub struct $ident(());
         )*
-        features! { @pack $($ident)* => [$($ident)*] }
+
+        macro_rules! feature_as_type {
+            $(
+                {
+                    $detect
+                } => {
+                    $ident
+                };
+            )*
+        }
+
+        tuple_impl! { $($ident)* }
+
+        features! { @pack $detect_macro, $([$detect, $ident])* => [$($ident)*] }
     };
 
     // This rule packs the list of feature idents into a token tree, so they can be iterated later
     {
-        @pack $($ident:ident)* => $all:tt
+        @pack $detect_macro:ident, $([$detect:tt, $ident:ident])* => $all:tt
     } => {
         $(
-            features! { @unpack $ident => $all }
+            features! { @unpack $detect_macro, $detect, $ident => $all }
         )*
     };
 
     // This rule unpacks the token tree to implement all of the traits
     {
-        @unpack $ident:ident => [$($all:ident)*]
+        @unpack $detect_macro:ident, $detect:tt, $ident:ident => [$($all:ident)*]
     } => {
         // This macro generates the Supports trait for just $ident
-        macro_rules! generate_trait {
+        macro_rules! generate_associated_type {
             // This is the target type!
             {
                 $ident
             } => {
-                unsafe impl Supports<Arch<$ident>> for Arch<$ident> {
-                    type Value = True;
-                }
+                type $ident = True;
             };
 
             // This is another type
             {
                 $other:ident
             } => {
-                unsafe impl Supports<Arch<$ident>> for Arch<$other> {
-                    type Value = False;
-                }
+                type $other = False;
             }
         }
 
-        // Generate the Supports trait for each feature
-        $(
-            generate_trait! { $all }
-        )*
+        unsafe impl Features for $ident {
+            $(
+                generate_associated_type! { $all }
+            )*
+
+            fn detect() -> Option<Self> {
+                if $detect_macro!($detect) {
+                    Some(Self(()))
+                } else {
+                    None
+                }
+            }
+
+            unsafe fn new_unchecked() -> Self {
+                Self(())
+            }
+        }
     }
 }
 
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
 features! {
     @detect_macro is_x86_feature_detected
 
@@ -87,98 +198,25 @@ features! {
     @detect "avx2"
 }
 
-pub unsafe trait Supports<T> {
-    type Value;
-}
-
-struct Arch<T>(std::marker::PhantomData<T>);
-struct And<T, U>((std::marker::PhantomData<T>, std::marker::PhantomData<U>));
-
-// A pair of architectures support the target if either one supports it
-unsafe impl<Target, Arch1, Arch2> Supports<Arch<Target>> for And<Arch1, Arch2>
-where
-    Arch1: Supports<Arch<Target>>,
-    Arch2: Supports<Arch<Target>>,
-    Arch1::Value: BitOr<Arch2::Value>,
-{
-    type Value = <Arch1::Value as BitOr<Arch2::Value>>::Output;
-}
-
-// A single architecture supports a pair of targets if it supports both individually
-// The architecture must be wrapped in a 1-tuple to deconflict with the previous impl
-unsafe impl<Target1, Target2, Arch> Supports<And<Target1, Target2>> for Arch
-where
-    Arch: Supports<Target1> + Supports<Target2>,
-    <Arch as Supports<Target1>>::Value: BitAnd<<Arch as Supports<Target2>>::Value>,
-{
-    type Value =
-        <<Arch as Supports<Target1>>::Value as BitAnd<<Arch as Supports<Target2>>::Value>>::Output;
-}
-
-pub unsafe trait Detect: Sized {
-    fn detect() -> Option<Self>;
-
-    unsafe fn new_unchecked() -> Self;
-}
-
-unsafe impl<T, U> Detect for And<T, U>
-where
-    T: Detect,
-    U: Detect,
-{
-    fn detect() -> Option<Self> {
-        if let (Some(_), Some(_)) = (T::detect(), U::detect()) {
-            Some(Self(Default::default()))
-        } else {
-            None
-        }
-    }
-
-    unsafe fn new_unchecked() -> Self {
-        Self(Default::default())
-    }
-}
-
 #[cfg(test)]
 mod test {
     use super::*;
 
+    #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
     #[test]
-    fn provide_one_feature() {
-        fn foo(_: impl Supports<Arch<sse>, Value = True>) {}
-        if let Some(tag) = Arch::<sse>::detect() {
-            foo(tag);
+    fn feature_requirement() {
+        fn foo<F>(_: F)
+        where
+            F: Features<sse = True, avx = True>,
+        {
+            println!(
+                "sse: {}, avx: {}, avx2: {}",
+                F::sse::BOOL,
+                F::avx::BOOL,
+                F::avx2::BOOL
+            );
         }
-    }
-
-    #[test]
-    fn provide_two_features() {
-        fn foo(_: impl Supports<Arch<sse>, Value = True>) {}
-        if let Some(tag) = <And<Arch<sse>, Arch<avx>>>::detect() {
-            foo(tag);
-        }
-    }
-
-    #[test]
-    fn provide_three_features() {
-        fn foo(_: impl Supports<Arch<sse>, Value = True>) {}
-        if let Some(tag) = <And<And<Arch<sse>, Arch<avx>>, Arch<avx>>>::detect() {
-            foo(tag);
-        }
-    }
-
-    #[test]
-    fn require_two_features() {
-        fn foo(_: impl Supports<And<Arch<sse>, Arch<avx>>, Value = True>) {}
-        if let Some(tag) = And::<Arch<sse>, Arch<avx>>::detect() {
-            foo(tag);
-        }
-    }
-
-    #[test]
-    fn require_three_features() {
-        fn foo(_: impl Supports<And<And<Arch<sse>, Arch<avx>>, Arch<avx2>>, Value = True>) {}
-        if let Some(tag) = And::<Arch<sse>, And<Arch<avx>, Arch<avx2>>>::detect() {
+        if let Some(tag) = <(sse, avx, avx2)>::detect() {
             foo(tag);
         }
     }
